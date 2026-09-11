4378e1ca6bba873d8aaf857d4182ee9e10bcbac7
Abhijeet Sharma <jazzyop7634@gmail.com>
2026-09-11 14:27:43 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> uniques = new HashSet<>();
+
+        for (int i : nums) {
+            if (!uniques.add(i)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
